466f9a6d1e73b774816bdce39f98511217014eb8
goni715 <halesgoni715149123@gmail.com>
2026-01-16 23:15:57 +0600

    input & output in cpp

---
+++ module-01/input_output.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+    //std::cout<<"Hello World";
+    //std::cout<<"Hello \n";
+    //std::cout<<"World";
+    //std::cout<<"Hello "<<"World"; //Hello World
+    //std::cout<<"Hello"<<std::endl<<"World";
+
+    int x=10;
+    int y=20;
+
+    //printf("%d", y); //20 in c
+    cout<<x<<y; //output= 1020
+    cout<<x<<" "<<y<<endl; 
+
+    return 0;
+}
