e1a13694bebc61fe9e81dbad285e52dd16797976
goni715 <halesgoni715149123@gmail.com>
2026-01-18 10:04:51 +0600

    summation of numbers

---
+++ module-01/print_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int x, y, z;
+    // x = 10;
+    // y = 20;
+    // z = 30;
+    // cout << x+y+z; //output=60
+
+    //assign the same value to multiple variables in one line
+    int x, y, z;
+    x = y = z = 50;
+    cout << x+y+z; //output = 150;
+
+    return 0;
+}
