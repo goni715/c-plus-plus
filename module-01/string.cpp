c8a07ccf6709d6d9d91b7121150cc2ffec3ff956
goni715 <halesgoni715149123@gmail.com>
2026-01-26 20:00:12 +0600

    string access & string function

--- module-01/string.cpp
+++ module-01/string.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int main() {
   string greeting = "Hello World";
-  cout << greeting;
+  //cout << greeting;
+
+  //concatenation
+  string firstName = "Osman";
+  string lastName = " Goni";
+  //string fullName = firstName + " "+lastName;
+  //cout<<fullName;
+
+  //use append for concatetion
+  string fullName = firstName.append(lastName);
+  //cout<<fullName;
+
+
+  string x = "10";
+  string y = "20";
+  string z = x + y; //output=1020
+  // cout << z;
+
+ //access string
+ //string myString = "Hello";
+ //cout << myString[0];
+ // Outputs= H
+
+
+ //string last value
+ string myString = "Hello";
+ cout << myString[myString.length() - 1];
+ // Outputs= o
   return 0;
 }
