95bb46042e1e1e2980f5fc93fcec1778f75a5813
goni715 <halesgoni715149123@gmail.com>
2026-01-27 23:55:43 +0600

    dynamic variable creation

---
+++ module-02/dynamic_variable.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *a = new int;
+    *a=20;
+    cout<<*a<<endl;
+
+    //for float number
+    float * f = new float;
+    *f=3.45;
+    cout<<*f<<endl;
+
+    return 0;
+}
