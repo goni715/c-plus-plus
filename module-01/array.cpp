7836e48038d12535d3771c27d3dd63112ce50139
goni715 <halesgoni715149123@gmail.com>
2026-01-31 22:43:40 +0600

    array looping

--- module-01/array.cpp
+++ module-01/array.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main(){
-
     string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    //cout<<cars[0];
 
-    cout<<cars[0];
+    for (int i = 0; i < 4; i++) {
+      cout << cars[i] << "\n";
+    }
 
     return 0;
 }
