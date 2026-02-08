d6af60904c9753afa20a8d61351a7767a7e09ddc
goni715 <halesgoni715149123@gmail.com>
2026-02-08 23:23:50 +0600

    string erase replace insert method

--- module-05-string/string_modifiers.cpp
+++ module-05-string/string_modifiers.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-    string x = "Hello";
-    string y = "World";
+    //string x = "Hello";
+    //string y = "World";
     //x = x+y;
     //x +=y; 
     //cout<<x<<endl; //output = HelloWorld
@@ -18,9 +18,20 @@ int main(){
    // a[5]='G' //didn't work, because index 6 is not available
 
    //push_back() = add character to string
-   x.push_back('G');
-   x.pop_back(); //remove last character
+   //x.push_back('G');
+   //x.pop_back(); //remove last character
    
+   //x="Hi";
+   //x.assign("Hi");
+   //cout<<x<<endl; //output= Hi
+
+   string x = "HelloWorld";
+   //x.erase(4); //output=Hell
+   //x.erase(4, 1); //output =HellWorld (o remove) = 1 ta remove
+   //x.erase(4, 3); //output= Hellrld = 3 ta remove
+   //x.replace(5, 0, "so"); //output= HellosoWorld
+   x.insert(5, "so"); //output= HellosoWorld
+   cout<<x<<endl;
 
     return 0;
 }
