d9356278ae46ddff0b8a5e2c158860bf05fa26fb
goni715 <halesgoni715149123@gmail.com>
2026-02-13 21:24:19 +0600

    string constructor & sort string

---
+++ module-o6/sort_string.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str = "fajtdktdv";
+    sort(str.begin(), str.end());
+    cout<<str; //output = addfjkttv
+    return 0;
+}
---
+++ module-o6/string_constructor.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //string s="Hello World";
+    //string s("Hello World"); //using constructor
+
+    //string s("Hello World", 4); // upto 4 index=Hell
+
+    //string a = "Hello World";
+    //string s(a, 4); //output =o World ( without 4 index);
+
+    string s(10, 'Y'); //10 Y = YYYYYYYYYY
+    string s(8, 'X'); //8 X = XXXXXXXX
+    cout<<s;
+    return 0;
+}
